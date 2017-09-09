dcb0ed4f894682ef18a68da8f9472b8793392c9c
Erick Toledo <erick.toledo@msg.com>
2017-09-08 20:48:38 -0400

    dded the term cost and signifiers for the values calculated *the cost* to not mix up *the count*

---
+++ javaEggshw.js
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Eggs
+{
+	public static void main(String[] args)
+	{
+		double dozenEggsprice = 3.25;
+		double singleEggsprice = 0.45;
+		int numb_of_Dozen = 12;
+		System.out.println(dozenEggsprice)
+		System.out.println(singleEggsprice)
+		System.out.println(numb_of_Dozen)
+
+    	/*Scanner imp = new Scanner (System.in);
+    	System.out.print("How many eggs do you need?");
+    	int eggs = imp.nextInt();
+    	double total_of_Dozen = (eggs/numb_of_Dozen)*dozenEggsprice;
+    	double total_of_Single = (eggs%numb_of_Dozen)*singleEggsprice;   
+    	double total_Cost = (total_of_Dozen + total_of_Single);
+                   
+ 	System.out.println("You ordered " + eggs + " eggs "); 
+ 	System.out.println("That's " + (total_of_Dozen) + " dozen at $" + dozenEggsprice + " per dozen. "); 
+ 	System.out.println("And that's " + (total_of_Single) + " loose eggs at $" + singleEggsprice + " cents each. ");
+ 	System.out.println("For a total of $" + total_Cost);*/
+	}
+}
